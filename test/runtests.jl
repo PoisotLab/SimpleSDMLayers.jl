3b525a33a4816cf9c65cc3857096b9336834d199
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-05-21 13:32:02 -0400

    :whie_check_mark: tests for basic construction

---
+++ test/runtests.jl
@@ -0,0 +1,28 @@
+using SimpleSDMLayers
+using Test
+
+global anyerrors = false
+
+tests = Dict{String,String}(
+                            "construction" => "construction.jl",
+                            "basics" => "basics.jl",
+                           )
+
+for (name,test) in tests
+   try
+      include(test)
+      println("\033[1m\033[32m✓\033[0m\t$(name)")
+   catch e
+      global anyerrors = true
+      println("\033[1m\033[31m×\033[0m\t$(name)")
+      println("\033[1m\033[38m→\033[0m\ttest/$(test)")
+      showerror(stdout, e, backtrace())
+      println()
+      break
+   end
+end
+
+if anyerrors
+   throw("Tests failed")
+end
+
