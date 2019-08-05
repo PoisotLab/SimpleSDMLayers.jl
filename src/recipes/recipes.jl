520bcac2d0d1ebba69cb04f3ac7f148dd6e03bde
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-08-05 13:41:26 -0400

    :sparkles: default plot is a heatmap

--- src/recipes/recipes.jl
+++ src/recipes/recipes.jl
@@ -1,11 +1,11 @@
 @recipe function plot(layer::T) where {T <: SimpleSDMLayer}
+   seriestype --> :heatmap
    @assert eltype(layer) <: Number
    if get(plotattributes, :seriestype, :heatmap) == :heatmap
       x --> longitudes(layer)
       y --> latitudes(layer)
       z --> layer.grid
-   end
-   if get(plotattributes, :seriestype, :histogram) in [:histogram, :density]
+   else if get(plotattributes, :seriestype, :histogram) in [:histogram, :density]
       filter(!isnan, layer.grid)
    end
 end
