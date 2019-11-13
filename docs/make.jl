a2c28b705d3a0028fc317db21a37311aa1012105
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-11-13 15:36:42 -0500

    :wrench: deploydoc
    as mentionned in #5

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,10 @@ push!(LOAD_PATH, joinpath("..", "src"))
 
 using Documenter, SimpleSDMLayers
 
-makedocs(sitename="Simple SDM Layers")
+makedocs(
+    sitename = "Simple SDM Layers"
+)
+
+deploydocs(
+    repo = "gtihub.com/EcoJulia/SimpleSDMLayers.jl.git"
+)
