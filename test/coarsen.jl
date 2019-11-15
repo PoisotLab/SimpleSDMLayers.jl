8652eaa54abe8a9e34aa2366256417084a24d38e
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-11-14 19:22:21 -0500

    :white_check_mark: adding a test for #10

--- test/coarsen.jl
+++ test/coarsen.jl
@@ -11,4 +11,8 @@ min22 = coarsen(S, minimum, (2,2))
 max33 = coarsen(S, maximum, (3,3))
 @test max33.grid == [15 33; 18 36]
 
+# Coarsen should play nicely with the non-float types
+M = SimpleSDMResponse(["a" NaN "b" "c"; "d" "e" "f" "g"; "d" "e" NaN "g"; NaN "x" "y" "z"], 0.0, 1.0, 0.0, 1.0)
+@info coarsen(M, x -> reduce(*, x)), (2,2))
+
 end
