bb02e4700a2953cb8df3b4d89a58acb8a5940598
Timothée <timothee.poisot@umontreal.ca>
2020-04-01 14:26:41 -0400

    :bug: correct sone type tests

--- src/lib/generated.jl
+++ src/lib/generated.jl
@@ -1,20 +1,12 @@
 import Base: maximum, minimum
 import Statistics: mean, median, std
 
-ops = ["Base.maximum", "Base.minimum", "Statistics.mean", "Statistics.median", "Statistics.std"]
+ops = ("Base.maximum", "Base.minimum", "Statistics.mean", "Statistics.median", "Statistics.std")
 
-for op = Symbol.(ops)
-    for ty in [:SimpleSDMPredictor, :SimpleSDMResponse]
-        eval(quote
-            """
-                $op(l::$ty{T,K}) where {T <: Number, K <: AbstractFloat}
-
-            Overload of `$op` for objects of type $ty - this applies $op to the
-            non-NaN elements of the `grid`.
-            """
-            function $op(l::$ty{T}) where {T <: Number}
-                return $op(filter(!isnan, l.grid))
-            end
-        end)
-    end
+for op in Symbol.(ops), ty in (:SimpleSDMResponse, :SimpleSDMPredictor)
+    eval(quote
+        function $op(l::$ty{T}) where {T <: Number}
+            return $op(filter(!isnan, l.grid))
+        end
+    end)
 end
--- test/construction.jl
+++ test/construction.jl
@@ -5,11 +5,26 @@ using Test
 # Correct type
 M = rand(Bool, (5,5))
 S = SimpleSDMPredictor(M, 0.0, 1.0, 0.0, 1.0)
-@test typeof(S) == SimpleSDMPredictor{Bool, Float64}
+@test typeof(S) == SimpleSDMPredictor{Bool}
 
 # Correct type
 M = rand(Bool, (5,5))
 R = SimpleSDMResponse(M, 0.0, 1.0, 0.0, 1.0)
-@test typeof(R) == SimpleSDMResponse{Bool, Float64}
+@test typeof(R) == SimpleSDMResponse{Bool}
+
+# Construction only from a matrix
+R = SimpleSDMResponse(M)
+@test typeof(R) == SimpleSDMResponse{Bool}
+@test R.left = -180.
+@test R.right = -180.
+@test R.top = 90.
+@test R.bottom = -90.
+
+R = SimpleSDMPredictor(M)
+@test typeof(R) == SimpleSDMPredictor{Bool}
+@test R.left = -180.
+@test R.right = -180.
+@test R.top = 90.
+@test R.bottom = -90.
 
 end
