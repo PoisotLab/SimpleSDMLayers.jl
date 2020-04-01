55cd3e61ab59be11396b202d5f6c5ce64432449f
Timothée <timothee.poisot@umontreal.ca>
2020-04-01 14:11:22 -0400

    :boom: both types LOSE one parameter for the coordinates

--- src/lib/generated.jl
+++ src/lib/generated.jl
@@ -5,15 +5,16 @@ ops = ["Base.maximum", "Base.minimum", "Statistics.mean", "Statistics.median", "
 
 for op = Symbol.(ops)
     for ty in [:SimpleSDMPredictor, :SimpleSDMResponse]
-    eval(quote
-        """
-            $op(l::$ty{T,K}) where {T <: Number, K <: AbstractFloat}
+        eval(quote
+            """
+                $op(l::$ty{T,K}) where {T <: Number, K <: AbstractFloat}
 
-        Overload of `$op` for objects of type $ty - this applies $op to the
-        non-NaN elements of the `grid`.
-        """
-        function $op(l::$ty{T,K}) where {T <: Number, K <: AbstractFloat}
-            return $op(filter(!isnan, l.grid))
-        end
-    end)
+            Overload of `$op` for objects of type $ty - this applies $op to the
+            non-NaN elements of the `grid`.
+            """
+            function $op(l::$ty{T}) where {T <: Number}
+                return $op(filter(!isnan, l.grid))
+            end
+        end)
+    end
 end
