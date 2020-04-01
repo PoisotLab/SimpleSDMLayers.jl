a4b1a37101e61e252e7b8de8fd90742b43d479dd
Timothée <timothee.poisot@umontreal.ca>
2020-04-01 15:17:52 -0400

    :bug: fix the getindex bug and draft doc

--- src/integrations/GBIF.jl
+++ src/integrations/GBIF.jl
@@ -11,11 +11,11 @@ Extracts the value of a layer at a given position for a `GBIFRecord`. If the
 `GBIFRecord` has no latitude or longitude, this will return `NaN`.
 """
 function Base.getindex(p::T, occurrence::GBIF.GBIFRecord) where {T <: SimpleSDMLayer}
-   @info occurrrence.latitude
-   @info occurrrence.longitude
+   @info occurrence.latitude
+   @info occurrence.longitude
    ismissing(occurrence.latitude) && return NaN
    ismissing(occurrence.longitude) && return NaN
-   return p[occurrence.latitude, occurrence.longitude]
+   return p[occurrence.longitude, occurrence.latitude]
 end
 
 """
@@ -25,11 +25,7 @@ Changes the values of the cell including the point at the requested latitude and
 longitude.
 """
 function Base.setindex!(p::SimpleSDMResponse{T}, v::T, occurrence::GBIF.GBIFRecord) where {T}
-   @info "lat $(getfield(occurrence, :latitude, missing))"
-   @info "lon $(getfield(occurrence, :longitude, missing))"
-   @info "lat2 $(occurrence.latitude)"
-   @info "lon2 $(occurrence.longitude)"
-   ismissing(getfield(occurrence, :latitude, missing)) && return nothing
-   ismissing(getfield(occurrence, :longitude, missing)) && return nothing
-   setindex!(p, v, occurrence.latitude, occurence.longitude)
+   ismissing(occurrence.latitude) && return nothing
+   ismissing(occurrence.longitude) && return nothing
+   setindex!(p, v, occurrence.longitude, occurrence.latitude)
 end
