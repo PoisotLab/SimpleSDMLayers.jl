497052714b32efe01d66f6370be6a433de597b3a
Timothée <timothee.poisot@umontreal.ca>
2020-04-01 14:03:46 -0400

    :sparkles: add generated functions

---
+++ src/lib/generated.jl
@@ -0,0 +1,19 @@
+import Base: maximum, minimum
+import Statistics: mean, median, std
+
+ops = ["Base.maximum", "Base.minimum", "Statistics.mean", "Statistics.median", "Statistics.std"]
+
+for op = Symbol.(ops)
+    for ty in [:SimpleSDMPredictor, :SimpleSDMResponse]
+    eval(quote
+        """
+            $op(l::$ty{T,K}) where {T <: Number, K <: AbstractFloat}
+
+        Overload of `$op` for objects of type $ty - this applies $op to the
+        non-NaN elements of the `grid`.
+        """
+        function $op(l::$ty{T,K}) where {T <: Number, K <: AbstractFloat}
+            return $op(filter(!isnan, l.grid))
+        end
+    end)
+end
