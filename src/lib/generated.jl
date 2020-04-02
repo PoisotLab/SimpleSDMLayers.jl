07cb65bb22a1540935106678bd87a319c202ca72
Timothée <timothee.poisot@umontreal.ca>
2020-04-02 15:33:49 -0400

    :memo: doc fixed

--- src/lib/generated.jl
+++ src/lib/generated.jl
@@ -13,8 +13,8 @@ for op in ops, ty in (:SimpleSDMResponse, :SimpleSDMPredictor)
 
         Applies `$($op)` to an object of type `$($ty)`. This function has been
         automatically generated. Note that this function is only applied to the
-        non-`NaN` elements of the layer, and has no method to work on the
-        `dims`; the grid itself can be extracted with `convert(Matrix, l)`.
+        non-`NaN` elements of the layer, and has no method to work on the `dims`
+        keyword; the grid itself can be extracted with `convert(Matrix, l)`.
         """
         function $op(l::$ty{T}) where {T <: Number}
             return $op(filter(!isnan, l.grid))
