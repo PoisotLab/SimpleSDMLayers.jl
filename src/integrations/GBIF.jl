c1e28bd6d5436f4f5cb65714e237b9d530b012e0
Timothée <timothee.poisot@umontreal.ca>
2020-04-01 21:34:35 -0400

    :sparkles: getindex for GBIFRecords

--- src/integrations/GBIF.jl
+++ src/integrations/GBIF.jl
@@ -28,6 +28,12 @@ function Base.setindex!(p::SimpleSDMResponse{T}, v::T, occurrence::GBIF.GBIFReco
    setindex!(p, v, occurrence.longitude, occurrence.latitude)
 end
 
+"""
+    clip(p::T, r::GBIF.GBIFRecords)
+
+Returns a clipped version (with a 5% margin) around all occurences in a
+GBIFRecords collection.
+"""
 function clip(p::T, r::GBIF.GBIFRecords)
    occ_latitudes = filter!(ismissing, [o.latitude for o in r])
    occ_longitudes = filter!(ismissing, [o.longitude for o in r])
@@ -44,4 +50,13 @@ function clip(p::T, r::GBIF.GBIFRecords)
    lon_min = max(p.left, (0.05*lon_Δ)-lon_min)
 
    return p[left=lon_min, right=lon_max, bottom=lat_min, top=lat_max]
+end
+
+"""
+    Base.getindex(p::T, r::GBIF.GBIFRecords) where {T <: SimpleSDMLayer}
+
+Returns the values of a layer at all occurrences in a `GBIFRecords` collection.
+"""
+function Base.getindex(p::T, r::GBIF.GBIFRecords) where {T <: SimpleSDMLayer}
+   return [p[o] for o in r]
 end
