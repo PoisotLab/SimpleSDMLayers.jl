53e79c5531ac8adb4c650f58a32ec1bed36196e2
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-07-09 21:37:43 -0400

    fix generated functions

---
+++ test/generated.jl
@@ -0,0 +1,15 @@
+module SSLTestGenerated
+using SimpleSDMLayers
+using Test
+
+M = rand(Float64, (5, 10))
+S = SimpleSDMPredictor(M, 0.0, 1.0, 0.0, 1.0)
+
+@test maximum(S) ≤ 1.0
+@test minimum(S) ≥ 0.0
+
+@test mean(S) == mean(M)
+@test std(S) == std(M)
+@test median(S) == median(M)
+
+end
