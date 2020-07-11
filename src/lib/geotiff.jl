6ed4d2b505909e933d200f6bef87444db474b618
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-07-11 14:32:40 -0400

    start working on reading only part of the file

--- src/lib/geotiff.jl
+++ src/lib/geotiff.jl
@@ -1,9 +1,15 @@
 """
-    geotiff(tiff_file; T::Type=Float64)
+    geotiff(tiff_file; left::T=-180.0, right::T=180.0, bottom::T=-90.0, top::T=90.0) where {T <: Number}
 
 The geotiff function reads a geotiff file, and returns it as a matrix of the correct type.
 """
-function geotiff(tiff_file; T::Type=Float64)
+function geotiff(tiff_file; left::T=-180.0, right::T=180.0, bottom::T=-90.0, top::T=90.0) where {T <: Number}
+    @assert right > left
+    @assert top > bottom
+    @assert left <= 180.0
+    @assert right >= -180.0
+    @assert top <= 90.0
+    @assert bottom >= -90.0
     ArchGDAL.read(tiff_file) do dataset
         band = ArchGDAL.getband(dataset, 1)
         width = ArchGDAL.width(dataset)
