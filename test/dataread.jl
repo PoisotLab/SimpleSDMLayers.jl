9b1d97767a764b2deaac788bc84f131d4bdf51cc
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-07-15 12:35:48 -0400

    remove Makefile

--- test/dataread.jl
+++ test/dataread.jl
@@ -6,7 +6,7 @@ file = joinpath(dirname(pathof(SimpleSDMLayers)), "data", "connectivity.tiff")
 struct MyConnectivityMap <: SimpleSDMLayers.SimpleSDMSource end
 SimpleSDMLayers.latitudes(::Type{MyConnectivityMap}) = (-10.0, 10.0)
 SimpleSDMLayers.longitudes(::Type{MyConnectivityMap}) = (-20.0, 20.0)
-mp = raster(SimpleSDMResponse, MyConnectivityMap(), file)
+mp = SimpleSDMLayers.raster(SimpleSDMResponse, MyConnectivityMap(), file)
 
 @test typeof(mp) <: SimpleSDMResponse
 @test size(mp) = (1255, 1206)
