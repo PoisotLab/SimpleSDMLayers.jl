4bb3d37ed863910e29b9ca5ad8507ff3f1a65c38
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-07-15 12:01:15 -0400

    fix test bug

--- test/dataread.jl
+++ test/dataread.jl
@@ -3,7 +3,7 @@ using SimpleSDMLayers
 using Test
 
 file = joinpath(dirname(pathof(SimpleSDMLayers)), "data", "connectivity.tiff")
-struct MyConnectivityMap <: SimpleSDMLayers.SimpleSDMSource nd
+struct MyConnectivityMap <: SimpleSDMLayers.SimpleSDMSource end
 SimpleSDMLayers.latitudes(::Type{MyConnectivityMap}) = (-10.0, 10.0)
 SimpleSDMLayers.longitudes(::Type{MyConnectivityMap}) = (-20.0, 20.0)
 mp = raster(SimpleSDMResponse, MyConnectivityMap(), file)
