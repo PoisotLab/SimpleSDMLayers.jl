5767c6bff42701c225015350bfa9c09d0a7fac26
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-07-20 11:56:31 -0400

    add tests for multiple layers overloads

--- test/generated.jl
+++ test/generated.jl
@@ -19,4 +19,11 @@ M = ones(Float64, (5, 10))
 S = SimpleSDMPredictor(M, 0.0, 1.0, 0.0, 1.0)
 @test typeof(sqrt(S)) <: SimpleSDMLayer
 
+S1 = SimpleSDMResponse([-1 0 1 nothing], 0.0, 1.0, 0.0, 1.0)
+S2 = SimpleSDMPredictor([Float32.([-2.0 1.0 1.0])... nothing], 0.0, 1.0, 0.0, 1.0)
+Sx = [S1, S2]
+@test reduce(min, Sx).grid == [-2 0 1 nothing]
+@test mean(Sx).grid == [Float32.([-1.5 0.5 1.0])... nothing]
+@test reduce(+, Sx).grid == [-3 1 2 nothing]
+
 end
