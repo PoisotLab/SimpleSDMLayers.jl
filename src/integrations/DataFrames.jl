854c0fb1c72ef627989540248e3796bb5a21028e
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-07-20 16:29:00 -0400

    add clip overload

--- src/integrations/DataFrames.jl
+++ src/integrations/DataFrames.jl
@@ -15,4 +15,33 @@ function Base.getindex(layer::T, df::DataFrames.DataFrame; latitude = :latitude,
     lons = df[:, longitude]
     return [layer[lon, lat] for (lon, lat) in zip(lons, lats)]
 end
+
+"""
+    clip(p::T, r::DataFrames.DataFrame)
+
+Returns a clipped version (with a 10% margin) around all occurences in a
+`DataFrame`.
+"""
+function SimpleSDMLayers.clip(layer::T, df::DataFrames.DataFrame; latitude = :latitude, longitude = :longitude) where {T <: SimpleSDMLayer}
+   occ_latitudes = filter(!ismissing, df[:, latitude])
+   occ_longitudes = filter(!ismissing, df[:, longitude])
+
+   lat_min = minimum(occ_latitudes)
+   lat_max = maximum(occ_latitudes)
+   lon_min = minimum(occ_longitudes)
+   lon_max = maximum(occ_longitudes)
+
+   lat_Δ = abs(lat_max - lat_min)
+   lon_Δ = abs(lon_max - lon_min)
+
+   scaling = 0.1
+   lon_s = scaling*lon_Δ
+   lat_s = scaling*lat_Δ
+
+   lat_max = min(layer.top, lat_max+lat_s)
+   lat_min = max(layer.bottom, lat_min-lat_s)
+   lon_max = min(layer.right, lon_max+lon_s)
+   lon_min = max(layer.left, lon_min-lon_s)
+
+   return layer[left=lon_min, right=lon_max, bottom=lat_min, top=lat_max]
 end
