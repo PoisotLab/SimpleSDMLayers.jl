e0ba544033f039319fcae50f2c953f1d1a06193a
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-08 11:19:38 -0500

    vcat/hcat test

--- test/overloads.jl
+++ test/overloads.jl
@@ -48,4 +48,18 @@ Z = convert(SimpleSDMPredictor, Y)
 V = collect(Z)
 @test typeof(V) == Vector{eltype(Z)}
 
+l1 = worldclim(1, left=0.0, right=10.0, bottom=0.0, right=10.0)
+l2 = worldclim(1, left=0.0, right=10.0, bottom=10.0, right=20.0)
+l3 = worldclim(1, left=10.0, right=20.0, bottom=0.0, right=10.0)
+l4 = worldclim(1, left=10.0, right=20.0, bottom=10.0, right=20.0)
+
+ml1 = hcat(l1, l3)
+vl1 = vcat(l1, l2)
+ml2 = hcat(l2, l4)
+vl2 = hcat(l3, l4)
+
+@test all(vcat(ml1, ml2).grid == hcat(vl1, vl2).grid)
+
+
+
 end
