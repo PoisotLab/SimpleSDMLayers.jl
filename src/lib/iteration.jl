5d58491a6721cd00218de73d3b5611037b7e1706
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-19 21:35:17 -0500

    ✨ iteration (Fixes #51)

---
+++ src/lib/iteration.jl
@@ -0,0 +1,20 @@
+function Base.getindex(layer::T, i::CartesianIndex{2}) where {T <: SimpleSDMLayer}
+    return layer.grid[i]
+end
+
+function Base.iterate(layer::T) where {T <: SimpleSDMLayer}
+    length(layer) == 0 && return nothing
+    idx = findfirst(!isnothing, layer.grid)
+    return (layer[idx], LinearIndices(layer.grid)[idx])
+end
+
+function Base.iterate(layer::T, state) where {T <: SimpleSDMLayer}
+    state == prod(size(S)) && return nothing
+    idx = findnext(!isnothing, layer.grid, CartesianIndices(layer.grid)[state+1])
+    isnothing(idx) && return nothing
+    return (layer[idx], LinearIndices(layer.grid)[idx])
+end
+
+function Base.length(layer::T) where {T <: SimpleSDMLayer}
+    return count(!isnothing, layer.grid)
+end
