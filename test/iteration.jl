419ce2f4c91bbccdec226c2d1eff633e7034b958
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-20 09:38:31 -0500

    🐛 fix tests

--- test/iteration.jl
+++ test/iteration.jl
@@ -5,7 +5,7 @@ using Test
 S = SimpleSDMPredictor([1 2 3; 4 5 6; 7 nothing 9], 0.0, 1.0, 0.0, 1.0)
 
 val = sort([v for v in S])
-@test val == vec([1 2 3 4 5 6 7 9])
-@test val == collect(S)
+@test all(val .== vec([1 2 3 4 5 6 7 9]))
+@test all(val .== sort(collect(S)))
 
 end
