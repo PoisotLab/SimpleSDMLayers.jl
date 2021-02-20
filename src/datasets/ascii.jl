a87c149864bc273d84770c12d38496d50bca8a3b
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-20 13:28:00 -0500

    ⚡ single find operation in _get_asc_field

--- src/datasets/ascii.jl
+++ src/datasets/ascii.jl
@@ -1,6 +1,6 @@
 function _get_asc_field(lines, field, type)
-    field_line = first(filter(line -> startswith(line, field), lines))
-    return parse(type, last(split(field_line, " "))), findfirst(line -> startswith(line, field), lines)
+    line_id = findfirst(line -> startswith(line, field), lines)
+    return parse(type, last(split(lines[line_id], " "))), line_id
 end
 
 """
