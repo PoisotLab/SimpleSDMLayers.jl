b08972152b9cbc239ccef132d1a57552eed55a6e
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-19 22:26:01 -0500

    🐛 fix test

--- test/iteration.jl
+++ test/iteration.jl
@@ -1,9 +1,8 @@
-module SSLTestBasics
+module SSLTestIteration
 using SimpleSDMLayers
 using Test
 
-M = [1 2 3; 4 5 6; 7 nothing 9]
-S = SimpleSDMPredictor(M, 0.0, 1.0, 0.0, 1.0)
+S = SimpleSDMPredictor([1 2 3; 4 5 6; 7 nothing 9], 0.0, 1.0, 0.0, 1.0)
 
 val = sort([v for v in S])
 @test val == vec([1 2 3 4 5 6 7 9])
