9671d3ef6b67224de1b6278d7891038714decc19
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-20 22:11:41 -0500

    ✔️ add the test

--- src/SimpleSDMLayers.jl
+++ src/SimpleSDMLayers.jl
@@ -34,7 +34,7 @@ include(joinpath("operations", "coarsen.jl"))
 include(joinpath("operations", "sliding.jl"))
 include(joinpath("operations", "mask.jl"))
 include(joinpath("operations", "rescale.jl"))
-export coarsen, slidingwindow, mask, rescale!
+export coarsen, slidingwindow, mask, rescale!, rescale
 
 include(joinpath("recipes", "recipes.jl"))
 
