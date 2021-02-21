e7183bf64222ac21331ad8346475f4a0c4a1b0c9
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-20 22:34:40 -0500

    ✨ rescale by quantiles

--- src/operations/rescale.jl
+++ src/operations/rescale.jl
@@ -20,7 +20,7 @@ in-place.
 """
 function rescale!(layer::TI, t::Tuple{T,T}) where {TI <: SimpleSDMLayer, T <: Number}
     occ = findall(!isnothing, layer.grid)
-    layer.grid[isnothing] .= _rescale(layer.grid[isnothing], t...)
+    layer.grid[occ] .= _rescale(layer.grid[occ], t...)
 end
 
 """
@@ -41,4 +41,31 @@ Copying version of `rescale!`.
 function rescale(layer::TI, t::Tuple{T,T}) where {TI <: SimpleSDMLayer, T <: Number}
     l = copy(layer)
     return rescale!(l, t)
+end
+
+
+"""
+    rescale!(layer::T, p::Vector{Real}) where {T <: SimpleSDMLayer}
+
+Rescale the values of a `layer` so that they match with the quantiles given in
+`p`. Internally, this uses the `Statistics.quantile` function.
+"""
+function rescale!(layer::T, p::Vector{TI}) where {T <: SimpleSDMLayer, TI <: AbstractFloat}
+    q = reverse!(quantile(layer, p))
+    occupied = findall(!isnothing, layer.grid)
+    v = collect(layer)
+    for i in 1:length(q)
+        layer.grid[occupied[findall(x -> x <= q[i], v)]] .= reverse(p)[i]
+    end
+    return layer
+end 
+
+"""
+    rescale(layer::T, p::Vector{Real}) where {T <: SimpleSDMLayer}
+
+Copying version of `rescale!`.
+"""
+function rescale(layer::T, p::Vector{TI}) where {T <: SimpleSDMLayer, TI <: AbstractFloat}
+    l = copy(layer)
+    return rescale!(l, p)
 end
