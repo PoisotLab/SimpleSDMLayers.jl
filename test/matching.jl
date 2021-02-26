a193b615842e821b98b0444f9278167171276a74
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-26 13:50:37 -0500

    🟢 increase testing for matching

--- test/matching.jl
+++ test/matching.jl
@@ -3,13 +3,18 @@ using SimpleSDMLayers
 using Test
 
 M = rand(Bool, 6, 9)
+for rep in 1:10
+    l = rand()
+    r = 4l
+    b = rand()
+    t = 12b
+    S = SimpleSDMPredictor(M, l, r, b, t)
 
-S = SimpleSDMPredictor(M, rand(), 4rand(), 3rand(), 16rand())
-
-@test SimpleSDMLayers._match_latitude(S, S.bottom) == 1
-@test SimpleSDMLayers._match_latitude(S, S.top) == 6
-@test SimpleSDMLayers._match_longitude(S, S.left) == 1
-@test SimpleSDMLayers._match_longitude(S, S.right) == 9
+    @test SimpleSDMLayers._match_latitude(S, S.bottom) == 1
+    @test SimpleSDMLayers._match_latitude(S, S.top) == 6
+    @test SimpleSDMLayers._match_longitude(S, S.left) == 1
+    @test SimpleSDMLayers._match_longitude(S, S.right) == 9
+end
 
 S = SimpleSDMResponse(rand(Bool, 4, 4), 0.3, 0.7, 0.5, 0.9)
 
@@ -18,6 +23,9 @@ S = SimpleSDMResponse(rand(Bool, 4, 4), 0.3, 0.7, 0.5, 0.9)
 @test SimpleSDMLayers._match_latitude(S, 0.5) == 1
 for (i,l) in enumerate(latitudes(S))
     @test SimpleSDMLayers._match_latitude(S, l) == i
+end
+for (i,l) in enumerate(longitudes(S))
+    @test SimpleSDMLayers._match_longitude(S, l) == i
 end
 
 end
