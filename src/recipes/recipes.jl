eb5fd34fa2a410cc3df256d14838cf8d7ecced9a
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-26 13:36:52 -0500

    🔧 plots

--- src/recipes/recipes.jl
+++ src/recipes/recipes.jl
@@ -4,15 +4,24 @@ test 1
 @recipe function plot(layer::T) where {T <: SimpleSDMLayer}
     eltype(layer) <: Number || throw(ArgumentError("Plotting is only supported for layers with number values ($(eltype(layer)))"))
     seriestype --> :heatmap
-    if get(plotattributes, :seriestype, :heatmap) in [:heatmap, :contour, :surface]
+    if get(plotattributes, :seriestype, :heatmap) in [:heatmap, :contour]
         aspect_ratio --> 1
         xlims --> extrema(longitudes(layer))
         ylims --> extrema(latitudes(layer))
         lg = copy(layer.grid)
         replace!(lg, nothing => NaN)
+        lg = convert(Matrix{Float64}, lg)
         longitudes(layer), latitudes(layer), lg
     elseif get(plotattributes, :seriestype, :histogram) in [:histogram, :density]
         collect(layer)
+    elseif get(plotattributes, :seriestype, :surface) in [:surface, :wireframe]
+        aspect_ratio --> 1
+        xlims --> extrema(longitudes(layer))
+        ylims --> extrema(latitudes(layer))
+        lg = copy(layer.grid)
+        replace!(lg, nothing => minimum(layer))
+        lg = convert(Matrix{Float64}, lg)
+        longitudes(layer), latitudes(layer), lg
     end
 end
 
@@ -20,7 +29,8 @@ end
 test 2
 """
 @recipe function plot(l1::FT, l2::ST) where {FT <: SimpleSDMLayer, ST <: SimpleSDMLayer}
-    eltype(layer) <: Number || throw(ArgumentError("Plotting is only supported for layers with number values ($(eltype(layer)))"))
+    eltype(l1) <: Number || throw(ArgumentError("Plotting is only supported for layers with number values ($(eltype(l1)))"))
+    eltype(l2) <: Number || throw(ArgumentError("Plotting is only supported for layers with number values ($(eltype(l2)))"))
     seriestype --> :scatter
     if get(plotattributes, :seriestype, :scatter) in [:scatter, :histogram2d]
         SimpleSDMLayers._layers_are_compatible(l1, l2)
