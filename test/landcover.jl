fa28da6cfa888b73d148735769a03c59cd988379
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-03-04 14:18:35 -0500

    ✅ fix tests

--- test/landcover.jl
+++ test/landcover.jl
@@ -2,7 +2,7 @@ module SSLTestLandCover
 using SimpleSDMLayers
 using Test
 
-lc1 = landcover(1; left=-5.0, right=7.0, bottom=30.0, top=45.0)
+lc1 = SimpleSDMPredictor(EarthEnv{LandCover}, 1; left=-5.0, right=7.0, bottom=30.0, top=45.0)
 @test typeof(lc1) <: SimpleSDMPredictor
 
 end
