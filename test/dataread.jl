a98a4b48eab5b1f6520f563e25b87243012264d3
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-03-04 14:11:46 -0500

    ✋ fix geotiff test

--- test/dataread.jl
+++ test/dataread.jl
@@ -1,12 +1,13 @@
 module SSLTestDataRead
 using SimpleSDMLayers
 using Test
+using RasterDataSources
 
 file = joinpath(dirname(pathof(SimpleSDMLayers)), "..", "data", "connectivity.tiff")
-struct MyConnectivityMap <: SimpleSDMLayers.SimpleSDMSource end
+struct MyConnectivityMap <: RasterDataSources.RasterDataSource end
 SimpleSDMLayers.latitudes(::Type{MyConnectivityMap}) = (-10.0, 10.0)
 SimpleSDMLayers.longitudes(::Type{MyConnectivityMap}) = (-20.0, 20.0)
-mp = SimpleSDMLayers.raster(SimpleSDMResponse, MyConnectivityMap(), file)
+mp = SimpleSDMLayers.geotiff(SimpleSDMResponse, MyConnectivityMap, file)
 
 @test typeof(mp) <: SimpleSDMResponse
 @test size(mp) == (1255, 1206)
