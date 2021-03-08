50143cc11052acffc57bf42b4cb74e699d21b0fa
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-03-07 21:35:03 -0500

    📖 move GBIF integration to SDMs

--- docs/make.jl
+++ docs/make.jl
@@ -23,7 +23,6 @@ makedocs(
         ],
         "Examples" => [
             "Temperature data" => "examples/temperature.md",
-            "GBIF integration" => "examples/gbif.md",
             "DataFrames integration" => "examples/dataframes.md",
             "Importing raster data" => "examples/import.md",
             "Sliding window analysis" => "examples/slidingwindow.md",
@@ -31,6 +30,7 @@ makedocs(
             "Landcover consensus" => "examples/consensus.md"
         ],
         "Building SDMs" => [
+            "GBIF integration" => "sdm/gbif.md",
             "BIOCLIM from scratch" => "sdm/bioclim.md"
         ]
     ]
