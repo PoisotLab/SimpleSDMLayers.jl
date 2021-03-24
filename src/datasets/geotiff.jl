10c0fba836a29c8052c5fc9c216549719f28c5e3
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-03-23 21:56:41 -0400

    🎉 much simpler to load geotiff

--- src/datasets/geotiff.jl
+++ src/datasets/geotiff.jl
@@ -8,7 +8,7 @@ function _find_span(n, m, M, pos)
 end
 
 """
-    geotiff(::Type{LT}, tiff_file; left::T=-180.0, right::T=180.0, bottom::T=-90.0, top::T=90.0) where {LT <: SimpleSDMLayer, T <: Number}
+    geotiff(::Type{LT}, tiff_file; left=nothing, right=nothing, bottom=nothing, top=nothing) where {LT <: SimpleSDMLayer}
 
 The geotiff function reads a geotiff file, and returns it as a matrix of the
 correct type. The optional arguments `left`, `right`, `bottom`, and `left` are
@@ -19,61 +19,65 @@ The first argument is the type of the `SimpleSDMLayer` to be returned.
 """
 function geotiff(
     ::Type{LT},
-    ::Type{ST},
-    tiff_file;
-    left = nothing,
-    right = nothing,
-    bottom = nothing,
-    top = nothing,
-) where {LT<:SimpleSDMLayer,ST<:SimpleSDMSource}
-
-    left = isnothing(left) ? minimum(longitudes(ST)) : left
-    right = isnothing(right) ? maximum(longitudes(ST)) : right
-    bottom = isnothing(bottom) ? minimum(latitudes(ST)) : bottom
-    top = isnothing(top) ? maximum(latitudes(ST)) : top
-
-    # We do a bunch of checking that the required bounding box is not out of bounds
-    # for the range of latitudes and longitudes.
-    @assert right > left
-    @assert top > bottom
+    file::AbstractString;
+    left = -180.0,
+    right = 180.0,
+    bottom = -90.0,
+    top = 90.0
+) where {LT<:SimpleSDMLayer}
 
     # This next block is reading the geotiff file, but also making sure that we
     # clip the file correctly to avoid reading more than we need.
-    ArchGDAL.read(tiff_file) do dataset
+    # This next block is reading the geotiff file, but also making sure that we
+    # clip the file correctly to avoid reading more than we need.
+    layer = ArchGDAL.read(file) do dataset
+
+        transform = ArchGDAL.getgeotransform(dataset)
+        wkt = ArchGDAL.getproj(dataset)
 
         # The data we need is pretty much always going to be stored in the first
         # band, so this is what we will get for now. Note that this is not
         # reading the data yet, just retrieving the metadata.
         band = ArchGDAL.getband(dataset, 1)
+        T = ArchGDAL.pixeltype(band)
+        nodata = convert(T, ArchGDAL.getnodatavalue(band))
 
-        # This next bit of information is crucial, as it will allow us to assign
-        # a matrix of the correct size, but also to get the right latitudes and
-        # longitudes.
+        # Get the correct latitudes
+        minlon = transform[1]
+        maxlat = transform[4]
+        maxlon = minlon + size(band,1)*transform[2]
+        minlat = maxlat - abs(size(band,2)*transform[6])
+
+        left = isnothing(left) ? minlon : max(left, minlon)
+        right = isnothing(right) ? maxlon : min(right, maxlon)
+        bottom = isnothing(bottom) ? minlat : max(bottom, minlat)
+        top = isnothing(top) ? maxlat : min(top, maxlat)
+
+        lon_stride, lat_stride = transform[2], transform[6]
+        
         width = ArchGDAL.width(dataset)
         height = ArchGDAL.height(dataset)
 
-        global lon_stride, lat_stride
-        global left_pos, right_pos
-        global bottom_pos, top_pos
+        #global lon_stride, lat_stride
+        #global left_pos, right_pos
+        #global bottom_pos, top_pos
 
-        lon_stride, left_pos, min_width = _find_span(width, minimum(longitudes(ST)), maximum(longitudes(ST)), left)
-        _, right_pos, max_width = _find_span(width, minimum(longitudes(ST)), maximum(longitudes(ST)), right)
-
-        lat_stride, top_pos, max_height = _find_span(height, minimum(latitudes(ST)), maximum(latitudes(ST)), top)
-        _, bottom_pos, min_height = _find_span(height, minimum(latitudes(ST)), maximum(latitudes(ST)), bottom)
+        lon_stride, left_pos, min_width = _find_span(width, minlon, maxlon, left)
+        _, right_pos, max_width = _find_span(width, minlon, maxlon, right)
+        lat_stride, top_pos, max_height = _find_span(height, minlat, maxlat, top)
+        _, bottom_pos, min_height = _find_span(height, minlat, maxlat, bottom)
 
         max_height, min_height = height .- (min_height, max_height) .+ 1
 
         # We are now ready to initialize a matrix of the correct type.
-        pixel_type = ArchGDAL.pixeltype(band)
-        buffer = Matrix{pixel_type}(undef, length(min_width:max_width), length(min_height:max_height))
+        buffer = Matrix{T}(undef, length(min_width:max_width), length(min_height:max_height))
         ArchGDAL.read!(dataset, buffer, 1, min_height:max_height, min_width:max_width)
+        buffer = convert(Matrix{Union{Nothing,eltype(buffer)}}, rotl90(buffer))
+        replace!(buffer, nodata => nothing)
+        LT(buffer, left_pos-0.5lon_stride, right_pos+0.5lon_stride, bottom_pos-0.5lat_stride, top_pos+0.5lat_stride)
     end
 
-    buffer = convert(Matrix{Union{Nothing,eltype(buffer)}}, rotl90(buffer))
-    replace!(buffer, minimum(buffer) => nothing)
-
-    return LT(buffer, left_pos-0.5lon_stride, right_pos+0.5lon_stride, bottom_pos-0.5lat_stride, top_pos+0.5lat_stride)
+    return layer
 
 end
 
