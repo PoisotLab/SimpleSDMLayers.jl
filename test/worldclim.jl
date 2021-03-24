aeabdbbae153c7897940c8609310e5ac9ab33a80
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-03-24 15:24:58 -0400

    🟢 add tests for datasets

--- test/worldclim.jl
+++ test/worldclim.jl
@@ -17,4 +17,13 @@ wc3 = SimpleSDMPredictor(WorldClim, BioClim, 3)
 wcrange = SimpleSDMPredictor(WorldClim, BioClim, 1:5)
 @test eltype(wcrange) <: SimpleSDMPredictor
 
+future = SimpleSDMPredictor(WorldClim, BioClim, MIROC6, SSP126, 1)
+@test typeof(future) <: SimpleSDMPredictor
+
+future = SimpleSDMPredictor(WorldClim, BioClim, CanESM5, SSP126, 1:3)
+@test eltype(future) <: SimpleSDMPredictor
+
+future = SimpleSDMPredictor(WorldClim, BioClim, CanESM5, SSP370, [1,2,5]; year="2061-2080")
+@test eltype(future) <: SimpleSDMPredictor
+
 end
