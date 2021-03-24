c72f8d4a05bf59619256dd1b5b1f3cbcd99a873b
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-03-24 15:30:55 -0400

    🟢 add more tests

--- test/chelsa.jl
+++ test/chelsa.jl
@@ -2,7 +2,13 @@ module SSLTestCHELSA
 using SimpleSDMLayers
 using Test
 
-lc1 = bioclim(1; left=-5.0, right=7.0, bottom=30.0, top=45.0)
+lc1 = SimpleSDMPredictor(CHELSA, BioClim, 1; left=-5.0, right=7.0, bottom=30.0, top=45.0)
 @test typeof(lc1) <: SimpleSDMPredictor
+
+lc1 = SimpleSDMPredictor(CHELSA, BioClim, 1:2; left=-5.0, right=7.0, bottom=30.0, top=45.0)
+@test eltype(lc1) <: SimpleSDMPredictor
+
+lc1 = SimpleSDMPredictor(CHELSA, BioClim, [1,2,3]; left=-5.0, right=7.0, bottom=30.0, top=45.0)
+@test eltype(lc1) <: SimpleSDMPredictor
 
 end
