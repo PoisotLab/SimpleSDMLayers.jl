68f4146e57407d8c065960860c9a879a9beda840
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-03-23 22:00:21 -0400

    🟢 fix test

--- test/dataread.jl
+++ test/dataread.jl
@@ -3,10 +3,7 @@ using SimpleSDMLayers
 using Test
 
 file = joinpath(dirname(pathof(SimpleSDMLayers)), "..", "data", "connectivity.tiff")
-struct MyConnectivityMap <: SimpleSDMLayers.SimpleSDMSource end
-SimpleSDMLayers.latitudes(::Type{MyConnectivityMap}) = (-10.0, 10.0)
-SimpleSDMLayers.longitudes(::Type{MyConnectivityMap}) = (-20.0, 20.0)
-mp = SimpleSDMLayers.raster(SimpleSDMResponse, MyConnectivityMap(), file)
+mp = geotiff(SimpleSDMResponse, file)
 
 @test typeof(mp) <: SimpleSDMResponse
 @test size(mp) == (1255, 1206)
