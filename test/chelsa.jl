e3d3a82e3ecec2b8e5df2856b7add363919fddfe
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-03-25 13:05:33 -0400

    📦 first 0.5 release

--- test/chelsa.jl
+++ test/chelsa.jl
@@ -11,4 +11,14 @@ lc1 = SimpleSDMPredictor(CHELSA, BioClim, 1:2; left=-5.0, right=7.0, bottom=30.0
 lc1 = SimpleSDMPredictor(CHELSA, BioClim, [1,2,3]; left=-5.0, right=7.0, bottom=30.0, top=45.0)
 @test eltype(lc1) <: SimpleSDMPredictor
 
+lc1 = SimpleSDMPredictor(CHELSA, BioClim, CanESM2, RCP26, 1; left=-5.0, right=7.0, bottom=30.0, top=45.0)
+@test typeof(lc1) <: SimpleSDMPredictor
+
+lc1 = SimpleSDMPredictor(CHELSA, BioClim, CanESM2, RCP26, 1:2; left=-5.0, right=7.0, bottom=30.0, top=45.0)
+@test eltype(lc1) <: SimpleSDMPredictor
+
+lc1 = SimpleSDMPredictor(CHELSA, BioClim, CanESM2, RCP26, [1,2,3]; left=-5.0, right=7.0, bottom=30.0, top=45.0)
+@test eltype(lc1) <: SimpleSDMPredictor
+
+
 end
