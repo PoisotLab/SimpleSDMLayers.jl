893ab8cc00a00e8ec38dd5d6c38115f3456da5bb
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-03-25 14:05:36 -0400

    📖 docstrings

--- src/datasets/chelsa/bioclim.jl
+++ src/datasets/chelsa/bioclim.jl
@@ -17,29 +17,6 @@ It is recommended to *keep* the content of the `path` folder, as it will
 eliminate the need to download the tiff files (which are quite large). For
 example, calling `bioclim(1:19)` will download and everything, and future
 calls will be much faster.
-
-| Variable | Description                                                |
-| ------   | ------                                                     |
-| 1        | Annual Mean Temperature                                    |
-| 2        | Mean Diurnal Range (Mean of monthly (max temp - min temp)) |
-| 3        | Isothermality (BIO2/BIO7) (* 100)                          |
-| 4        | Temperature Seasonality (standard deviation *100)          |
-| 5        | Max Temperature of Warmest Month                           |
-| 6        | Min Temperature of Coldest Month                           |
-| 7        | Temperature Annual Range (BIO5-BIO6)                       |
-| 8        | Mean Temperature of Wettest Quarter                        |
-| 9        | Mean Temperature of Driest Quarter                         |
-| 10       | Mean Temperature of Warmest Quarter                        |
-| 11       | Mean Temperature of Coldest Quarter                        |
-| 12       | Annual Precipitation                                       |
-| 13       | Precipitation of Wettest Month                             |
-| 14       | Precipitation of Driest Month                              |
-| 15       | Precipitation Seasonality (Coefficient of Variation)       |
-| 16       | Precipitation of Wettest Quarter                           |
-| 17       | Precipitation of Driest Quarter                            |
-| 18       | Precipitation of Warmest Quarter                           |
-| 19       | Precipitation of Coldest Quarter                           |
-
 """
 function SimpleSDMPredictor(::Type{CHELSA}, ::Type{BioClim}, layer::Integer=1; kwargs...)
     file = _get_raster(CHELSA, BioClim, layer)
--- src/datasets/types.jl
+++ src/datasets/types.jl
@@ -1,13 +1,129 @@
+
+"""
+    LayerProvider
+
+A `LayerProvider` is an abstract type used to dispatch the correct call of
+`SimpleSDMPredictor` to a specific dataset. A dataset is specified by a
+`LayerProvider` and a `LayerDataset`, as well as optionally one or multiple
+layers, and future climate information.
+"""
 abstract type LayerProvider end
+
+"""
+    LayerDataset
+
+A `LayerDataset` is a specific set of rasters provided by a `LayerProvider`.
+"""
 abstract type LayerDataset end
 
+"""
+    WorldClim
+
+TODO WorldClim
+
+This provider currently offers `BioClim` data, both historical and future under
+`CMIP6`.
+"""
 struct WorldClim <: LayerProvider end
+
+"""
+    CHELSA
+
+TODO CHELSA
+
+This provider currently offers `BioClim` data, both historical and future under
+`CMIP5`.
+"""
 struct CHELSA <: LayerProvider end
+
+"""
+    EarthEnv
+
+Data from the earthenv project, all released under a CC-BY-NC license to Tuanmu
+& Jetz. This provider currently offers `LandCover` and `HabitatHeterogeneity`
+rasters.
+"""
 struct EarthEnv <: LayerProvider end
 
+"""
+    BioClim
+
+A list of 19 bioclimatic variables derived from the monthly temperature and
+precipitation data. This dataset is provided by `WorldClim` and `CHELSA`, both
+of which offer future versions under `CMIP5` and `CMIP6` models.
+
+| Variable | Description                                                |
+| ------   | ------                                                     |
+| 1        | Annual Mean Temperature                                    |
+| 2        | Mean Diurnal Range (Mean of monthly (max temp - min temp)) |
+| 3        | Isothermality (BIO2/BIO7) (* 100)                          |
+| 4        | Temperature Seasonality (standard deviation *100)          |
+| 5        | Max Temperature of Warmest Month                           |
+| 6        | Min Temperature of Coldest Month                           |
+| 7        | Temperature Annual Range (BIO5-BIO6)                       |
+| 8        | Mean Temperature of Wettest Quarter                        |
+| 9        | Mean Temperature of Driest Quarter                         |
+| 10       | Mean Temperature of Warmest Quarter                        |
+| 11       | Mean Temperature of Coldest Quarter                        |
+| 12       | Annual Precipitation                                       |
+| 13       | Precipitation of Wettest Month                             |
+| 14       | Precipitation of Driest Month                              |
+| 15       | Precipitation Seasonality (Coefficient of Variation)       |
+| 16       | Precipitation of Wettest Quarter                           |
+| 17       | Precipitation of Driest Quarter                            |
+| 18       | Precipitation of Warmest Quarter                           |
+| 19       | Precipitation of Coldest Quarter                           |
+"""
 struct BioClim <: LayerDataset end
+
+"""
+    LandCover
+
+Information on land cover, currently only provided by `EarthEnv`.
+"""
 struct LandCover <: LayerDataset end
+
+"""
+    HabitatHeterogeneity
+
+Information on habitat heterogeneity, currently only provided by `EarthEnv`.
+"""
 struct HabitatHeterogeneity <: LayerDataset end
+
+"""
+    SharedSocioeconomicPathway
+
+Enumeration of the four SSPs, which can be listed with
+`instances(SharedSocioeconomicPathway)`. These are meant to be used with `CMIP6`
+models.
+"""
+@enum SharedSocioeconomicPathway SSP126 SSP245 SSP370 SSP585
+
+"""
+    RepresentativeConcentrationPathway
+
+Enumeration of the four RCPs, which can be listed with
+`instances(RepresentativeConcentrationPathway)`. These are meant to be used with
+`CMIP5` models.
+"""
+@enum RepresentativeConcentrationPathway RCP26 RCP45 RCP60 RCP85
+
+"""
+    CMIP6
+
+Enumeration of the models from CMIP6, which can be listed with
+`instances(CMIP6)`.
+"""
+@enum CMIP6 BCCCSM2MR CNRMCMS61 CNRMESM21 CanESM5 GFDLESM4 IPSLCM6ALR MIROCES2L MIROC6 MRIESM2
+
+"""
+    CMIP5
+
+Enumeration of the models from CMIP5, which can be listed with
+`instances(CMIP5)`.
+"""
+@enum CMIP5 ACCESS10 BNUESM CCSM4 CESM1BGC CESM1CAM5 CMCCCMS CMCCCM CNRMCM5 CSIROMK360 CanESM2 FGOALSG2 FIOESM GFDLCM3 GFDLESM2G GFDLESM2M GISSE2HCC GISSE2H GISSE2RCC GISSE2R HADGEM2AO HADGEM2CC IPSLCM5ALR IPSLCM5AMR MIROCESMCHEM MIROCESM MIROC5 MPIESMLR MPIESMMR MRICGCM3 MRIESM1 NORESM1M BCCCSM11 INMCM4
+
 
 # Provider paths
 _rasterpath(::Type{WorldClim}) = "WorldClim"
@@ -19,13 +135,7 @@ _rasterpath(::Type{BioClim}) = "BioClim"
 _rasterpath(::Type{LandCover}) = "LandCover"
 _rasterpath(::Type{HabitatHeterogeneity}) = "HabitatHeterogeneity"
 
-# Future climate information as enum types
-@enum SharedSocioeconomicPathway SSP126 SSP245 SSP370 SSP585
-@enum RepresentativeConcentrationPathway RCP26 RCP45 RCP60 RCP85
-
-@enum CMIP6 BCCCSM2MR CNRMCMS61 CNRMESM21 CanESM5 GFDLESM4 IPSLCM6ALR MIROCES2L MIROC6 MRIESM2
-@enum CMIP5 ACCESS10 BNUESM CCSM4 CESM1BGC CESM1CAM5 CMCCCMS CMCCCM CNRMCM5 CSIROMK360 CanESM2 FGOALSG2 FIOESM GFDLCM3 GFDLESM2G GFDLESM2M GISSE2HCC GISSE2H GISSE2RCC GISSE2R HADGEM2AO HADGEM2CC IPSLCM5ALR IPSLCM5AMR MIROCESMCHEM MIROCESM MIROC5 MPIESMLR MPIESMMR MRICGCM3 MRIESM1 NORESM1M BCCCSM11 INMCM4
-
+# Future paths
 _rasterpath(model::CMIP6) = _rasterpath(Val{model})
 _rasterpath(model::CMIP5) = _rasterpath(Val{model})
 _rasterpath(ssp::SharedSocioeconomicPathway) = _rasterpath(Val{ssp})
