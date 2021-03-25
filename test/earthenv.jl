eabe2767cd27cc2bd56937802adc1a89820579e1
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-03-24 21:38:09 -0400

    🟢 additional tests for EarthEnv

--- test/earthenv.jl
+++ test/earthenv.jl
@@ -5,7 +5,16 @@ using Test
 lc1 = SimpleSDMPredictor(EarthEnv, LandCover, 1; left=-5.0, right=7.0, bottom=30.0, top=45.0)
 @test typeof(lc1) <: SimpleSDMPredictor
 
+lc1 = SimpleSDMPredictor(EarthEnv, LandCover, 1; full=true, left=-5.0, right=7.0, bottom=30.0, top=45.0)
+@test typeof(lc1) <: SimpleSDMPredictor
+
+lc1 = SimpleSDMPredictor(EarthEnv, LandCover, 1:3; left=-5.0, right=7.0, bottom=30.0, top=45.0)
+@test eltype(lc1) <: SimpleSDMPredictor
+
 he1 = SimpleSDMPredictor(EarthEnv, HabitatHeterogeneity, 1; left=-5.0, right=7.0, bottom=30.0, top=45.0)
 @test typeof(he1) <: SimpleSDMPredictor
+
+he1 = SimpleSDMPredictor(EarthEnv, HabitatHeterogeneity, 1:3; left=-5.0, right=7.0, bottom=30.0, top=45.0)
+@test eltype(he1) <: SimpleSDMPredictor
 
 end
