6e90488a9be511e1f4bfd81d6d9eccef9d2491d2
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-01 14:14:31 -0400

    ✅ clip v. getindex

--- test/overloads.jl
+++ test/overloads.jl
@@ -13,24 +13,27 @@ for i in eachindex(S)
 end
 
 @test typeof(S[0.2, 0.6]) == eltype(M)
+@test S[0.2, 0.6] == S[Point(0.2, 0.6)]
 @test isnothing(S[1.2, 0.3])
 @test isnothing(S[1.2, 1.3])
 @test isnothing(S[0.2, 1.3])
 
-@test typeof(S[left=0.2, right=0.6, bottom=0.5, top=1.0]) == typeof(S)
-@test S[left=0.2, right=0.6, bottom=0.5, top=1.0].top ≈ 1.0
-@test S[left=0.2, right=0.6, bottom=0.5, top=1.0].bottom ≈ 0.4
-@test S[left=0.2, right=0.6, bottom=0.5, top=1.0].right ≈ 0.6
-@test S[left=0.19, right=0.6, bottom=0.5, top=1.0].left <= 0.2
+cl1 = clip(S; left=0.2, right=0.6, bottom=0.5, top=1.0)
+@test typeof(cl1) == typeof(S)
+@test cl1.top ≈ 1.0
+@test cl1.bottom ≈ 0.4
+@test cl1.right ≈ 0.6
+@test clip(S; left=0.19).left <= 0.2
 
-@test typeof(S[left=0.2, bottom=0.5]) == typeof(S)
-@test S[left=0.2, bottom=0.5].top ≈ 1.0
-@test S[left=0.2, bottom=0.5].bottom ≈ 0.4
-@test S[left=0.2, bottom=0.5].right ≈ 1.0
-@test S[left=0.2, bottom=0.5].left ≈ 0.2
+cl2 = clip(S; left=0.2, bottom=0.5)
+@test typeof(cl2) == typeof(S)
+@test cl2.top ≈ 1.0
+@test cl2.bottom ≈ 0.4
+@test cl2.right ≈ 1.0
+@test cl2.left ≈ 0.2
 
 C = (left=0.2, bottom=0.5)
-@test typeof(S[C]) == typeof(S)
+@test typeof(clip(S; C...)) == typeof(S)
 
 Y = SimpleSDMResponse(zeros(Float64, (5,5)), 0.0, 1.0, 0.0, 1.0)
 Y[0.1,0.1] = 0.2
