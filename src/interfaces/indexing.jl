257d0b058b25e8a719174d634ad8d71304ccae2f
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-01 12:52:58 -0400

    ✨ more getindex methods

--- src/interfaces/indexing.jl
+++ src/interfaces/indexing.jl
@@ -1,7 +1,9 @@
 function _point_to_cartesian(layer::T, c::Point) where {T <: SimpleSDMLayer}
-    lat = SimpleSDMLayers._match_latitude(layer, GeoInterface.ycoord(c))
-    lon = SimpleSDMLayers._match_longitude(layer, GeoInterface.xcoord(c))
-    return CartesianIndex(lon, lat)
+    lon = SimpleSDMLayers._match_longitude(layer, c[1])
+    lat = SimpleSDMLayers._match_latitude(layer, c[2])
+    isnothing(lon) && return nothing
+    isnothing(lat) && return nothing
+    return CartesianIndex(lat, lon)
 end
 
 function Base.CartesianIndices(layer::T) where {T <: SimpleSDMLayer}
@@ -24,16 +26,8 @@ function Base.getindex(layer::T, i::Vector{CartesianIndex{2}}) where {T <: Simpl
     return layer.grid[i]
 end
 
-function Base.setindex!(layer::T, v, i::CartesianIndex{2}) where {T <: SimpleSDMResponse}
-    return setindex!(layer.grid, v, i)
-end
-
-function Base.setindex!(layer::T, v, i::Array{CartesianIndex{2}}) where {T <: SimpleSDMLayer}
-    return layer.grid[i] = v
-end
-
-function Base.getindex(layer::T, i::AbstractFloat, j::AbstractFloat) where {T <: SimpleSDMLayer}
-    return layer[RasterCoordinate(i,j)]
+function Base.getindex(layer::T, longitude::AbstractFloat, latitude::AbstractFloat) where {T <: SimpleSDMLayer}
+    return layer[Point(longitude,latitude)]
 end
 
 function Base.getindex(layer::T, c::Point) where {T <: SimpleSDMLayer}
@@ -42,4 +36,16 @@ end
 
 function Base.getindex(layer::T, c::Array{<:Point}) where {T <: SimpleSDMLayer}
     return layer[[_point_to_cartesian(layer, i) for i in c]]
+end
+
+function Base.getindex(::T, ::Nothing) where {T <: SimpleSDMLayer}
+    return nothing
+end
+
+function Base.setindex!(layer::T, v, i::CartesianIndex{2}) where {T <: SimpleSDMResponse}
+    return setindex!(layer.grid, v, i)
+end
+
+function Base.setindex!(layer::T, v, i::Array{CartesianIndex{2}}) where {T <: SimpleSDMLayer}
+    return layer.grid[i] = v
 end
