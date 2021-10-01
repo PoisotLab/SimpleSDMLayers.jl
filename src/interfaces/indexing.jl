0628f469d382d5d5b51ed518d2accb5df7051e63
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-01 11:13:15 -0400

    ✨ getindex using coordinates

--- src/interfaces/indexing.jl
+++ src/interfaces/indexing.jl
@@ -18,3 +18,17 @@ function Base.setindex!(X::T, v, i::Array{CartesianIndex{2}}) where {T <: Simple
     @info "lul"
     return X.grid[i] = v
 end
+
+function _raster_to_cartesian(layer::T, c::RasterCoordinate) where {T <: SimpleSDMLayer}
+    lat = SimpleSDMLayers._match_latitude(layer, GeoInterface.ycoord(c))
+    lon = SimpleSDMLayers._match_longitude(layer, GeoInterface.xcoord(c))
+    return CartesianIndex(lon, lat)
+end
+
+function Base.getindex(X::T, c::RasterCoordinate) where {T <: SimpleSDMLayer}
+    return X[_raster_to_cartesian(X, c)]
+end
+
+function Base.getindex(X::T, c::Array{<:RasterCoordinate}) where {T <: SimpleSDMLayer}
+    return X[[_raster_to_cartesian(X, i) for i in c]]
+end
