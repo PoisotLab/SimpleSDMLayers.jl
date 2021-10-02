3dc9db1842ad8b0d36754e56047ba51d6758d454
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-02 18:35:43 -0400

    🟩 the bug was in broadcast all along

--- test/core/setindex.jl
+++ test/core/setindex.jl
@@ -13,10 +13,24 @@ S[changepoint] = 1.0
 changepoints = [Point(rand(2)...) for i in 1:8]
 changevalues = rand(length(changepoints))
 S[changepoints] = changevalues
-@test all(S[changepoints] .== changevalues)
+# NOTE we test that the values in the layer are part of the pool of possible
+# values, because it is entirely possible that two points will fall within the
+# same cell
+@test all(.!isnothing.(indexin(S[changepoints], changevalues)))
 
-# Change multiple by a single value
+# Change multiple by multiple values as a matrix
+changepoints = reshape([Point(rand(2)...) for i in 1:8], (2, 4))
+changevalues = reshape(rand(length(changepoints)), (2,4))
+S[changepoints] = changevalues
+@test all(.!isnothing.(indexin(S[changepoints], changevalues)))
+
+# Change multiple values by a single value
 changepoints = [Point(rand(2)...) for i in 1:8]
+S[changepoints] .= 5.0
+@test all(S[changepoints] .== 5.0)
+
+# Change multiple values by a single value as a matrix
+changepoints = reshape([Point(rand(2)...) for i in 1:8], (2, 4))
 S[changepoints] .= 5.0
 @test all(S[changepoints] .== 5.0)
 
