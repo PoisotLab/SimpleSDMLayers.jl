8b10591b7154816040c8cf276411d75f0b9020d0
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-03 08:52:02 -0400

    ⚙️ revert broadcast

--- src/interfaces/broadcast.jl
+++ src/interfaces/broadcast.jl
@@ -4,17 +4,15 @@ Base.BroadcastStyle(::Type{<:SimpleSDMResponse}) = Broadcast.Style{SimpleSDMResp
 Base.broadcastable(layer::SimpleSDMPredictor) = similar(layer)
 Base.BroadcastStyle(::Type{<:SimpleSDMPredictor}) = Broadcast.Style{SimpleSDMPredictor}()
 
-#=
-"""
-   Broadcast.broadcast(f, L::LT) where {LT <: SimpleSDMLayer}
-
-TODO
-"""
 function Base.Broadcast.broadcast(f, L::LT) where {LT <: SimpleSDMLayer}
-    newgrid = similar(L)
-    for i in L
-        newgrid[i.first] = f(i.second)
-    end
-    return newgrid
+    newgrid = Array{Any}(nothing, size(L))
+    N = SimpleSDMResponse(newgrid, L)
+    v = filter(!isnothing, L.grid)
+    fv = f.(v)
+    N.grid[findall(!isnothing, L.grid)] .= fv
+
+    internal_types = unique(typeof.(N.grid))
+
+    RT = LT <: SimpleSDMResponse ? SimpleSDMResponse : SimpleSDMPredictor
+    return RT(convert(Matrix{Union{internal_types...}}, N.grid), N)
 end
-=#
