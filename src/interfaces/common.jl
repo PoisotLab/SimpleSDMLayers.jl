2a7b85217f7716313cefa9a0b2c9212b0e658b3c
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-04 22:04:31 -0400

    ✨ add keys for layers

--- src/interfaces/common.jl
+++ src/interfaces/common.jl
@@ -14,3 +14,15 @@ Returns the size of the grid alongside a dimension.
 Base.size(layer::T, i...) where {T <: SimpleSDMLayer} = size(layer.grid, i...)
 
 Base.length(layer::T) where {T <: SimpleSDMLayer} = count(!isnothing, layer.grid)
+
+"""
+    Base.keys(layer::T) where {T <: SimpleSDMLayer}
+
+Returns an array of `Point` where every entry in the array is a non-`nothing`
+grid coordinate.
+"""
+function Base.keys(layer::T) where {T <: SimpleSDMLayer}
+    _lon = longitudes(layer)
+    _lat = latitudes(layer)
+    return [Point(_lon[p[2]], _lat[p[1]]) for p in findall(!isnothing, layer.grid)]
+end
--- src/lib/coordinateconversion.jl
+++ src/lib/coordinateconversion.jl
@@ -14,38 +14,40 @@ function _point_to_cartesian(layer::T, c::Point; side=:center) where {T <: Simpl
 end
 
 function _match_latitude(layer::T, lat::K; side=:none) where {T <: SimpleSDMLayer, K <: AbstractFloat}
-    side in [:none, :bottom, :top] || throw(ArgumentError("side must be one of :none (default), :bottom, :top"))
 
-    lat > layer.top && return nothing
-    lat < layer.bottom && return nothing
+    layer.top > lat > layer.bottom || return nothing
 
     ldiff = abs.(lat .- latitudes(layer))
     lapprox = isapprox.(ldiff, stride(layer, 2))
+
     if side == :none || !any(lapprox)
         l = last(findmin(ldiff))
     elseif side == :bottom
         l = findlast(lapprox)
     elseif side == :top
         l = findfirst(lapprox)
+    else
+        throw(ArgumentError("side must be one of :none (default), :bottom, :top"))
     end
     
     return l
 end
 
 function _match_longitude(layer::T, lon::K; side::Symbol=:none) where {T <: SimpleSDMLayer, K <: AbstractFloat}
-    side in [:none, :left, :right] || throw(ArgumentError("side must be one of :none (default), :left, :right"))
     
-    lon > layer.right && return nothing
-    lon < layer.left && return nothing
+    layer.right > lon > layer.left || return nothing
     
     ldiff = abs.(lon .- longitudes(layer))
     lapprox = isapprox.(ldiff, stride(layer, 1))
+    
     if side == :none || !any(lapprox)
         l = last(findmin(ldiff))
     elseif side == :left
         l = findlast(lapprox)
     elseif side == :right
         l = findfirst(lapprox)
+    else
+        throw(ArgumentError("side must be one of :none (default), :left, :right"))
     end
 
     return l
