d992666068eb0e96f99c926e53ce5e976619c2d2
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-05 10:16:03 -0400

    ⚙️ update matching code

--- src/lib/coordinateconversion.jl
+++ src/lib/coordinateconversion.jl
@@ -1,78 +1,44 @@
 function _point_to_cartesian(layer::T, c::Point; side=:center) where {T <: SimpleSDMLayer}
-    lonside, latside = :none, :none
+    lonside, latside = true, true
     if side == :bottomleft
-        lonside, latside = :left, :bottom
+        lonside, latside = true, true
     end
     if side == :topright
-        lonside, latside = :right, :top
+        lonside, latside = false, false
     end
-    lon = SimpleSDMLayers._match_longitude(layer, c[1]; side=lonside)
-    lat = SimpleSDMLayers._match_latitude(layer, c[2]; side=latside)
+    lon = SimpleSDMLayers._match_longitude(layer, c[1]; lower=lonside)
+    lat = SimpleSDMLayers._match_latitude(layer, c[2]; lower=latside)
     isnothing(lon) && return nothing
     isnothing(lat) && return nothing
     return CartesianIndex(lat, lon)
 end
 
-function _match_latitude(layer::T, lat::K; side=:none) where {T <: SimpleSDMLayer, K <: AbstractFloat}
-
-    layer.top >= lat >= layer.bottom || return nothing
-
-    relative = (lat - layer.bottom)/(layer.top - layer.bottom)*(size(layer,1))+1
-    
-    @assert side in [:bottom, :top, :none]
-
-    isequal(:none)(side) && return min(floor(Int, relative), size(layer, 1))
-    isequal(:bottom)(side) && return min(floor(Int64, relative), size(layer, 1))
-    isequal(:top)(side) && return min(floor(Int64, relative), size(layer, 1))
-
-    #=
-    rval = floor(Int64, relative):1:ceil(Int64, relative)
-
-    ldiff = abs.(lat .- latitudes(layer)[rval])
-    lapprox = isapprox.(ldiff, stride(layer, 2))
-
-    if side == :none || !any(lapprox)
-        l = last(findmin(ldiff))
-    elseif side == :bottom
-        l = findlast(lapprox)
-    elseif side == :top
-        l = findfirst(lapprox)
+function _match_latitude(layer::T, lat::K; lower::Bool=true) where {T <: SimpleSDMLayer, K <: AbstractFloat}
+    layer.bottom <= lat <= layer.top || return nothing
+    lat == layer.bottom  && return 1
+    lat == layer.top  && return size(layer, 1)
+    relative = (lat - layer.bottom)/(layer.top - layer.bottom)
+    fractional = relative * size(layer, 1)+1
+    if lat in layer.bottom:2stride(layer,2):layer.top
+        f = lower ? floor : ceil
+        d = lower ? 1 : 0
+        return min(f(Int64, fractional-d), size(layer, 1))
     else
-        throw(ArgumentError("side must be one of :none (default), :bottom, :top"))
+        return min(floor(Int, fractional), size(layer, 1))
     end
-    
-    return rval[l]
-    =#
 end
 
-function _match_longitude(layer::T, lon::K; side::Symbol=:none) where {T <: SimpleSDMLayer, K <: AbstractFloat}
-    
-    layer.right >= lon >= layer.left || return nothing
-    
-    relative = (lon - layer.left)/(layer.right - layer.left)*(size(layer,2))+1    
-    
-    @assert side in [:left, :right, :none]
-
-    isequal(:none)(side) && return min(floor(Int, relative), size(layer, 2))
-    isequal(:left)(side) && return min(floor(Int64, relative), size(layer, 2))
-    isequal(:right)(side) && return min(floor(Int64, relative), size(layer, 2))
-
-    #=
-    rval = floor(Int64, relative):1:ceil(Int64, relative)
-
-    ldiff = abs.(lon .- longitudes(layer)[rval])
-    lapprox = isapprox.(ldiff, stride(layer, 1))
-    
-    if side == :none || !any(lapprox)
-        l = last(findmin(ldiff))
-    elseif side == :left
-        l = findlast(lapprox)
-    elseif side == :right
-        l = findfirst(lapprox)
+function _match_longitude(layer::T, lon::K; lower::Bool=true) where {T <: SimpleSDMLayer, K <: AbstractFloat}
+    layer.left <= lon <= layer.top || return nothing
+    lon == layer.left  && return 1
+    lon == layer.right  && return size(layer, 2)
+    relative = (lon - layer.left)/(layer.right - layer.left)
+    fractional = relative * size(layer, 2)+1
+    if lon in layer.left:2stride(layer,1):layer.right
+        f = lower ? floor : ceil
+        d = lower ? 1 : 0
+        return min(f(Int64, fractional-d), size(layer, 2))
     else
-        throw(ArgumentError("side must be one of :none (default), :left, :right"))
+        return min(floor(Int, fractional), size(layer, 2))
     end
-
-    return rval[l]
-    =#
 end
