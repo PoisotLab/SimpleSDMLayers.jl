7b3e8b8d26d99ecd9a213f5f85c2fea7501bc985
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-08 18:23:17 -0400

    ✨ add layernames to identify the layers

---
+++ src/datasets/layernames.jl
@@ -0,0 +1,63 @@
+layernames(::Type{WorldClim}, ::Type{Elevation}) = ("Elevation",)
+
+function layernames(::Type{WorldClim}, ::Type{BioClim})
+    return (
+        "Annual Mean Temperature",
+        "Mean Diurnal Range",
+        "Isothermality",
+        "Temperature Seasonality",
+        "Max Temperature of Warmest Month",
+        "Min Temperature of Coldest Month",
+        "Temperature Annual Range",
+        "Mean Temperature of Wettest Quarter",
+        "Mean Temperature of Driest Quarter",
+        "Mean Temperature of Warmest Quarter",
+        "Mean Temperature of Coldest Quarter",
+        "Annual Precipitation",
+        "Precipitation of Wettest Month",
+        "Precipitation of Driest Month",
+        "Precipitation Seasonality",
+        "Precipitation of Wettest Quarter",
+        "Precipitation of Driest Quarter",
+        "Precipitation of Warmest Quarter",
+        "Precipitation of Coldest Quarter",
+    )
+end
+
+layernames(::Type{CHELSA}, ::Type{BioClim}) = layernames(WorldClim, BioClim)
+
+function layernames(::Type{EarthEnv}, ::Type{HabitatHeterogeneity})
+    return (
+        "Coefficient of variation",
+        "Evenness",
+        "Range",
+        "Shannon",
+        "Simpson",
+        "Standard deviation",
+        "Contrast",
+        "Correlation",
+        "Dissimilarity",
+        "Entropy",
+        "Homogeneity",
+        "Maximum",
+        "Uniformity",
+        "Variance",
+    )
+end
+
+function layernames(::Type{EarthEnv}, ::Type{LandCover})
+    return (
+        "Evergreen/Deciduous Needleleaf Trees",
+        "Evergreen Broadleaf Trees",
+        "Deciduous Broadleaf Trees",
+        "Mixed/Other Trees",
+        "Shrubs",
+        "Herbaceous Vegetation",
+        "Cultivated and Managed Vegetation",
+        "Regularly Flooded Vegetation",
+        "Urban/Built-up",
+        "Snow/Ice",
+        "Barren",
+        "Open Water",
+    )
+end
