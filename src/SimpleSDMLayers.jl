bce87fb0b3c711b197dee39ce96aef464cff2e7b
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-10 15:15:16 -0400

    ➕ add Colors and ColorBlendModes

--- src/SimpleSDMLayers.jl
+++ src/SimpleSDMLayers.jl
@@ -3,6 +3,7 @@ module SimpleSDMLayers
 using ArchGDAL
 using Downloads
 using RecipesBase
+using Colors, ColorBlendModes
 using ZipFile
 using Requires
 using Distances
--- src/recipes/recipes.jl
+++ src/recipes/recipes.jl
@@ -1,3 +1,39 @@
+
+@shorthands bivariate
+
+@recipe function f(::Type{Val{:bivariate}}, plt::AbstractPlot; classes=3, p0=colorant"#e8e8e8", p1=colorant"#64acbe", p2=colorant"#c85a5a")
+    x = plotattributes[:x]
+    y = plotattributes[:y]
+    println(plotattributes)
+
+    # Get the palettes
+    c1 = palette([p0, p1], classes)
+    c2 = palette([p0, p2], classes)
+    breakpoints = LinRange(0.0, 1.0, classes+1)
+
+    xlims --> extrema(longitudes(x))
+    ylims--> extrema(latitudes(x))
+    legend --> false
+    aspectratio --> 1
+    frame --> :box
+    subplot --> 1
+
+    for i in 2:length(breakpoints)
+        m1 = broadcast(v -> breakpoints[i - 1] <= v <= breakpoints[i], x)
+        for j in 2:length(breakpoints)
+            m2 = broadcast(v -> breakpoints[j - 1] <= v <= breakpoints[j], y)
+            m = reduce(*, [m1, m2])
+            replace!(m, false => nothing)
+            replace!(m, nothing => NaN)
+            @series begin
+                seriestype --> :heatmap
+                color --> BlendMultiply(c1[i - 1], c2[j - 1])
+                longitudes(m), latitudes(m), convert(Matrix{Float64}, m.grid)
+            end
+        end
+    end
+end
+
 """
 test 1
 """
