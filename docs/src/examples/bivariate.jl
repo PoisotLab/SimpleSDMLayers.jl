93d4633d7561d02c82e63322da0c1d7407c3beb2
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-10-10 21:26:44 -0400

    full bivariate example

--- docs/src/examples/bivariate.jl
+++ docs/src/examples/bivariate.jl
@@ -10,12 +10,16 @@ using StatsPlots.PlotMeasures
 # measures: eveness of land use, and terrain roughness. We will trasnform the
 # values into quantiles, as this is generally prefered for bivariate maps.
 
-boundaries = (left=-12.0, right=20.0, bottom=36.0, top=62.0)
-layer1 = convert(Float16, SimpleSDMPredictor(EarthEnv, HabitatHeterogeneity, 2; resolution=5, boundaries...))
-layer2 = convert(Float16, SimpleSDMPredictor(EarthEnv, Topography, 7; resolution=5, boundaries...))
+boundaries = (left=-12.0, right=30.0, bottom=36.0, top=72.0)
+layer1 = convert(
+    Float16,
+    SimpleSDMPredictor(EarthEnv, HabitatHeterogeneity, 2; resolution=5, boundaries...),
+)
+layer2 = convert(
+    Float16, SimpleSDMPredictor(EarthEnv, Topography, 7; resolution=5, boundaries...)
+)
 q1 = rescale(layer1, collect(LinRange(0.0, 1.0, 100)));
 q2 = mask(q1, rescale(layer2, collect(LinRange(0.0, 1.0, 100))));
-
 
 # Note that bivariate maps usually work best when used with 9 classes in total
 # (so 3 for each side). The next decision is to take a bivaraite color palette,
@@ -32,17 +36,17 @@ bv_pal_4 = (p0=p0, p1=colorant"#be64ac", p2=colorant"#5ac8c8")
 
 # The bivariate map itself is a call to plot:
 
-plot(q1, q2; st=:bivariate, bv_pal_4...)
+plot(q1, q2; st=:bivariate, bv_pal_3...)
 
 # Note that you can use the `bivariate` shorthand as well:
 
-bivariate(q1, q2; classes=3, frame=:box, bv_pal_2...)
+pl1 = bivariate(q1, q2; classes=3, frame=:box, bv_pal_4...)
 xaxis!(pl1, "Longitude")
 yaxis!(pl1, "Latitude")
 
 # We can repeat essentially the same process for the legend:
 
-pl2 = bivariatelegend(q1, q2; classes=3, bv_pal_2...)
+pl2 = bivariatelegend(q1, q2; classes=3, bv_pal_4...)
 xaxis!(pl2, layernames(EarthEnv, HabitatHeterogeneity)[2])
 yaxis!(pl2, layernames(EarthEnv, Topography)[7])
 
@@ -51,10 +55,20 @@ yaxis!(pl2, layernames(EarthEnv, Topography)[7])
 
 plot(pl1, pl2; layout=@layout [a{0.75w} b])
 
-# TK
+# Using the `subplot` and `inset` arguments of Plots.jl, we can have the legend
+# within the figure. Note how in this example we expand the limits on the x axis
+# to make the legend fit.
 
-
-p1 = bivariate(q1, q2; bv_pal_4..., classes=12, frame=:box, xlim=(-21, maximum(longitudes(q1))))
+p1 = bivariate(q1, q2; classes=3, bv_pal_4..., frame=:box, xlim=(-24, maximum(longitudes(q1))))
 xaxis!(p1, "Longitude")
 yaxis!(p1, "Latitude")
-p2 = bivariatelegend!(q1, q2; bv_pal_4..., inset = (1, bbox(0.01,0.1,0.28,0.28, :bottom, :left)), subplot = 2, xlab=layernames(WorldClim, BioClim)[1], ylab=layernames(WorldClim, BioClim)[12], guidefontsize=6)
+p2 = bivariatelegend!(
+    q1,
+    q2;
+    bv_pal_4...,
+    inset=(1, bbox(0.04, 0.05, 0.28, 0.28, :top, :left)),
+    subplot=2,
+    xlab=layernames(EarthEnv, HabitatHeterogeneity)[2],
+    ylab=layernames(EarthEnv, Topography)[7],
+    guidefontsize=7,
+)
