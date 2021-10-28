5930730edb55b9ec15a78de9ac271dea4623bc5a
michael catchen <michael.catchen@colorado.edu>
2021-10-28 16:38:42 -0400

    :pencil: docs and blue style

--- src/integrations/MultivariateStats.jl
+++ src/integrations/MultivariateStats.jl
@@ -3,44 +3,59 @@
 
 """
 	MultivariateStats.fit(a, layers::Vector{T}, kwargs...) where T <: SimpleSDMLayer
+
+	Overloads the `fit` function from `MultivariateStats.jl`.
 """
-function MultivariateStats.fit(a, layers::Vector{T}, kwargs...) where T <: SimpleSDMLayer
-	input = _make_input(layers)
-	pca = MultivariateStats.fit(a, input, kwargs...)
-	return pca
+function MultivariateStats.fit(
+    a::PT,
+    layers::Vector{T},
+    kwargs...,
+) where {
+    T<:SimpleSDMLayer,
+    PT<:Union{Type{MultivariateStats.PCA},Type{MultivariateStats.PPCA}},
+}
+    input = _make_input(layers)
+    pca = MultivariateStats.fit(a, input, kwargs...)
+    return pca
 end
 
 """
-	#
+	MultivariateStats.transform(proj::P, layers::Vector{T}) where {P,T<:SimpleSDMLayer}
+
+	Overload of the `transform` function from `MultivariateStats.jl`. 
 """
 function MultivariateStats.transform(proj::P, layers::Vector{T}) where {P,T<:SimpleSDMLayer}
-	input = _make_input(layers)
-	numsites = length(layers)
+    input = _make_input(layers)
+    numsites = length(layers)
 
-	A = MultivariateStats.projection(proj)
-	outdim = MultivariateStats.outdim(proj)
-	transformedvals = zeros(outdim, numsites)
-	
-	for loc in 1:numsites
-		transformedvals[:,loc] =  A' * vec(input[:,loc])
-	end
+    A = MultivariateStats.projection(proj)
+    outdim = MultivariateStats.outdim(proj)
+    transformedvals = zeros(outdim, numsites)
 
-	newlayers = [zeros(Float32, size(layers[begin])) for l in 1:outdim]
-	for loc in 1:numsites
-		for layer in 1:outdim
-			newlayers[layer][loc] =  transformedvals[layer,loc]
-		end
-	end
+    for loc = 1:numsites
+        transformedvals[:, loc] = A' * vec(input[:, loc])
+    end
 
-	newlayers = SimpleSDMPredictor.(newlayers, boundingbox(layers[begin])...)
-	newlayers = map(f -> rescale(f, (0,1)), newlayers)
+    newlayers = [zeros(Float32, size(layers[begin])) for l = 1:outdim]
+    for loc = 1:numsites
+        for layer = 1:outdim
+            newlayers[layer][loc] = transformedvals[layer, loc]
+        end
+    end
 
-	newlayers
+    newlayers = SimpleSDMPredictor.(newlayers, boundingbox(layers[begin])...)
+    return map(f -> rescale(f, (0, 1)), newlayers)
 end
 
+
+"""
+	_make_input(layers)
+
+	Creates a matrix for input to PCA. 
+	The resulting matrix has columns for each shared location in 
+	the vector of layers, where the value of each column is the 
+"""
 function _make_input(layers)
-	common_keys = reduce(∩, keys.(layers))
-	x = hcat([layer[common_keys] for layer in layers]...);
-	x
+    common_keys = reduce(∩, keys.(layers))
+    return hcat([layer[common_keys] for layer in layers]...)
 end
-
--- test/extensions/multivariatestats.jl
+++ test/extensions/multivariatestats.jl
@@ -4,13 +4,17 @@ module SSLTestMVStats
     using NeutralLandscapes
     using Test
 
-    layers = [SimpleSDMPredictor(rand(MidpointDisplacement(0.9), (50,50))) for i in 1:30]
+    TEST_DIMS = (150,150)
+    TEST_AUTOCORRELATION = 0.9
+    TEST_NUM_LAYERS = 10
+
+    layers = [SimpleSDMPredictor(rand(MidpointDisplacement(TEST_AUTOCORRELATION), TEST_DIMS...)) for i in 1:TEST_NUM_LAYERS]
 
 
     @assert typeof(transform(fit(PCA, layers),layers)) <: Vector{T} where T<:SimpleSDMLayer
     @assert typeof(transform(fit(PPCA, layers),layers)) <: Vector{T} where T<:SimpleSDMLayer
-    # @assert typeof(transform(fit(KernelPCA, layers),layers)) <: Vector{T} where T<:SimpleSDMLayer
-
-   # @assert typeof(transform(fit(Whitening, layers),layers)) <: Vector{T} where T<:SimpleSDMLayer
+    
+#    @assert typeof(transform(fit(KernelPCA, layers),layers)) <: Vector{T} where T<:SimpleSDMLayer
+#    @assert typeof(transform(fit(Whitening, layers),layers)) <: Vector{T} where T<:SimpleSDMLayer
 
 end
