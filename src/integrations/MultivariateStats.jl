f09e364fd937519e35b89a1e6ae07fcbc715da0d
michael catchen <michael.catchen@colorado.edu>
2021-11-04 17:16:41 -0400

    removing type assert

--- src/integrations/MultivariateStats.jl
+++ src/integrations/MultivariateStats.jl
@@ -6,24 +6,18 @@
 
 	Overloads the `fit` function from `MultivariateStats.jl`.
 """
-function MultivariateStats.fit(
-    a::PT,
-    layers::Vector{T},
-    kwargs...,
-) where {
-    T<:SimpleSDMLayer,
-    PT<:Union{Type{MultivariateStats.PCA},Type{MultivariateStats.PPCA}},
-}
-	_layers_are_compatible(layers) || return ArgumentError("layers not compatible")
+function MultivariateStats.fit(a, layers::Vector{T}, kwargs...) where {T<:SimpleSDMLayer}
+    _layers_are_compatible(layers) || return ArgumentError("layers are not compatible")
     input = _make_input(layers)
-    pca = MultivariateStats.fit(a, input, kwargs...)
-    return pca
+    proj = MultivariateStats.fit(a, input, kwargs...)
+    return proj
 end
 
 """
 	MultivariateStats.transform(proj::P, layers::Vector{T}) where {P,T<:SimpleSDMLayer}
 
-	Overload of the `transform` function from `MultivariateStats.jl`. 
+	Overload of the `transform` function from `MultivariateStats.jl`. Here `proj` is a 
+	and output object from `MultivariateStats.fit` (see above). 
 """
 function MultivariateStats.transform(proj::P, layers::Vector{T}) where {P,T<:SimpleSDMLayer}
     input = _make_input(layers)
@@ -55,6 +49,7 @@ end
 	Creates a matrix for input to PCA. 
 	The resulting matrix has columns for each shared location in 
 	the vector of layers, where the value of each column is the 
+	values of each of the `layers` input.
 """
 function _make_input(layers)
     common_keys = reduce(∩, keys.(layers))
--- test/extensions/multivariatestats.jl
+++ test/extensions/multivariatestats.jl
@@ -11,10 +11,13 @@ module SSLTestMVStats
     layers = [SimpleSDMPredictor(rand(MidpointDisplacement(TEST_AUTOCORRELATION), TEST_DIMS...)) for i in 1:TEST_NUM_LAYERS]
 
 
-    @assert typeof(transform(fit(PCA, layers),layers)) <: Vector{T} where T<:SimpleSDMLayer
-    @assert typeof(transform(fit(PPCA, layers),layers)) <: Vector{T} where T<:SimpleSDMLayer
-    
+    @test typeof(transform(fit(PCA, layers),layers)) <: Vector{T} where T<:SimpleSDMLayer
+    @test typeof(transform(fit(PPCA, layers),layers)) <: Vector{T} where T<:SimpleSDMLayer
+
+
+
 #    @assert typeof(transform(fit(KernelPCA, layers),layers)) <: Vector{T} where T<:SimpleSDMLayer
 #    @assert typeof(transform(fit(Whitening, layers),layers)) <: Vector{T} where T<:SimpleSDMLayer
 
 end
+
