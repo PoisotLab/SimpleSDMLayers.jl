1eeb82250817bdf80dfb208a6c250635e277590c
michael catchen <michael.catchen@colorado.edu>
2021-11-08 17:14:43 -0500

    :pencil: docs should make a plot?

--- docs/src/examples/pca.jl
+++ docs/src/examples/pca.jl
@@ -3,23 +3,22 @@
 using SimpleSDMLayers
 using MultivariateStats
 using Plots
-
+using Unitful
 
 # The `SimpleSDMLayers` enables integration with `MultivariateStats.jl` 
 # In this example, we will show how this can work.
 
-boundaries = (left=-12.0, right=30.0, bottom=36.0, top=72.0)
+boundaries =(left = -164.022167, right = -55.250858, bottom = 23.547132, top = 72.105833)
 
 layers = convert.(
     Float32,
     SimpleSDMPredictor(WorldClim, BioClim, 1:19; boundaries...),
 )
 
-plot(layers[10])
 
 pca = fit(PCA, layers)
 newlayers = transform(pca, layers)
 
 
-
-plot(plot.(newlayers, size=(300,300))...)
+pcaplots = plot.(newlayers)
+plot(pcaplots...)
--- src/integrations/MultivariateStats.jl
+++ src/integrations/MultivariateStats.jl
@@ -31,19 +31,5 @@ function MultivariateStats.transform(proj::P, layers::Vector{T}) where {P,T<:Sim
             newlayers[i][key] = pcaproj[i]
         end
     end
-    return newlayers
-end
-
-
-"""
-	_make_input(layers)
-
-	Creates a matrix for input to PCA. 
-	The resulting matrix has columns for each shared location in 
-	the vector of layers, where the value of each column is the 
-	values of each of the `layers` input.
-"""
-function _make_input(layers)
-    common_keys = reduce(∩, keys.(layers))
-    return hcat([layer[common_keys] for layer in layers]...)
+    return map(f->rescale(f, (0,1)),newlayers)
 end
