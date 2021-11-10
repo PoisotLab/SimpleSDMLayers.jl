2afab997ffdcbdbf7ca14676c887e32a232518ef
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-11-09 20:28:52 -0500

    🧹  no need to call SSDML.transform

--- src/SimpleSDMLayers.jl
+++ src/SimpleSDMLayers.jl
@@ -99,7 +99,7 @@ isdir(_layers_assets_path) || mkpath(_layers_assets_path)
 export clip
 
 function __init__()
-    @require GBIF="ee291a33-5a6c-5552-a3c8-0f29a1181037" begin
+    @require GBIF = "ee291a33-5a6c-5552-a3c8-0f29a1181037" begin
         @info "Loading GBIF support for SimpleSDMLayers.jl"
         include("integrations/GBIF.jl")
     end
@@ -111,7 +111,6 @@ function __init__()
     @require MultivariateStats = "6f286f6a-111f-5878-ab1e-185364afe411" begin
         @info "Loading MultivariateStats support for SimpleSDMLayers.jl"
         include("integrations/MultivariateStats.jl")
-        export transform
     end
 
 end
