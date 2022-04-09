04fce6752df8c71ef2b58ced6adbf4307e9a45c2
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-04-09 18:51:58 -0400

    temp code for stopping

--- faux.jl
+++ faux.jl
@@ -34,17 +34,25 @@ h_intra = fit(Histogram, vec(intrasp_distances) ./ max_distance, 0.0:0.05:1.0).w
 # Cells within d
 function randompoint(ref, d, R)
     λ, φ = deg2rad.(ref)
-    δ = d/R
+    δ = d / R
 
-    α = deg2rad(rand()*360.0)
-    φ2 = asin(sin(φ)*cos(δ) + cos(φ)*sin(δ)*cos(α))
-    λ2 = λ + atan(sin(α)*sin(δ)*cos(φ), cos(δ)-sin(φ)*sin(φ2))
+    α = deg2rad(rand() * 360.0)
+    φ2 = asin(sin(φ) * cos(δ) + cos(φ) * sin(δ) * cos(α))
+    λ2 = λ + atan(sin(α) * sin(δ) * cos(φ), cos(δ) - sin(φ) * sin(φ2))
     return rad2deg.((λ2, φ2))
 end
 
 # Initial point
-fauxpoints = [tuple(rand(keys(elevation))...)]
-d0 = Inf
+invalid = true
+while invalid
+    # Get a random distance
+    rdist = rand(intrasp_distances)
+    rcent = rand(fauxpoints)
+    new_faux = randompoint(rcent, rdist, _R)
+    invalid = isnothing(elevation[new_faux...])
+end
+fauxpoints = [new_faux]
+
 while length(fauxpoints) < length(coordinates)
     invalid = true
     while invalid
@@ -59,14 +67,16 @@ while length(fauxpoints) < length(coordinates)
     fd = pairwise(_dist, fauxpoints)
 
     if maximum(vec(fd)) > max_distance
-        @info "Distance too high"
+        @info "Point rejected (distance too high)"
         pop!(fauxpoints)
     end
 end
 
 progression = Float64[]
+counter = 0
+d0 = Inf
 
-for i in 1:5_000
+for i in 1:500_000
     rdist = rand(intrasp_distances)
     ridx = rand(1:length(fauxpoints))
     invalid = true
@@ -85,13 +95,19 @@ for i in 1:5_000
     if dt < d0
         d0 = dt
         @info "Score: $(d0)"
+        counter = 0
     else
         fauxpoints[ridx] = oldpoint
+        counter = counter + 1
     end
     push!(progression, d0)
+    if counter == 100
+        @info "No improvement for the last 100 steps, returning"
+        break
+    end
 end
 
 plot(progression, c=:black, lab="", dpi=400, lw=2)
 
-plot(h_intra ./ length(coordinates))
-plot!(h_faux ./ length(fauxpoints))
+plot(h_intra ./ sum(h_intra), m=:circle, c=:lightgrey, lab="Empirical distribution")
+scatter!(h_faux ./ sum(h_faux), lab="Simulated data")
