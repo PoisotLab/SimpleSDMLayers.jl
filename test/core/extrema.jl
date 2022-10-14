2fec766cf5612ddd27500cf6002aaf6b722e217d
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-10-14 11:31:29 -0400

    🥳 minor

--- test/core/extrema.jl
+++ test/core/extrema.jl
@@ -7,12 +7,12 @@ using Test
 S = SimpleSDMPredictor([1 2 3; 4 5 6; 7 nothing 9], 0.0, 1.0, 0.0, 1.0)
 
 @test extrema(S) == (1, 9)
-@info S[findmax(S)...]
-@info findmin(S)
+@test (findmin(S) |> first).second == first(extrema(S))
+@test (findmax(S) |> first).second == last(extrema(S))
 
 # Test 2
 
-bbox = (left=-83.0, bottom=46.4, right=-55.2, top=63.7);
+bbox = (left=-83.0, bottom=46.4, right=-55.2, top=63.7)
 temp = convert(Float32, SimpleSDMPredictor(WorldClim, BioClim, 7; bbox...))
 
 tempmin, tempmax = extrema(temp)
@@ -24,5 +24,8 @@ tempmin, tempmax = extrema(temp)
 
 @info findmin(temp)
 # > ([-82.91666666666667, 46.416666666666664] => 40.6895f0, [-82.91666666666667, 46.416666666666664])
+
+@test findmin(temp).second == first(extrema(temp))
+@test findmax(temp).second == last(extrema(temp))
 
 end
